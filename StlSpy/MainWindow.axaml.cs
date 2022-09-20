2e1b3a2558e272646aa73cecc047c2a6d595cca2
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2022-09-20 23:53:22 +0200

    Initial commit

---
+++ StlSpy/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace StlSpy
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
